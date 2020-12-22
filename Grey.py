2a9a62bac69cc7d60d622bf5f481ec3d5d889649
BuildTools <unconfigured@null.spigotmc.org>
2020-12-22 20:11:29 +0900

    commit

--- Grey.py
+++ Grey.py
@@ -2,12 +2,41 @@
 #await ctx.message.delete : 방금 보낸 텍스트 삭제
 #bot.command 뒤에 (pass_context = True, aliases =[]) 으로 여러개의 명령어가 같은 역할 수행하게 할 수 있음
 
+import random
 import discord
 from discord.ext import commands
+from discord.ext import tasks
 import os
 
 bot = commands.Bot(command_prefix='=')
 client = discord.Client()
+
+NoU = ["No U", "I am alive", "Grey is alive", "I didn't die", "You are a liar"]
+
+def grey_death_checker(message) :
+    content = message.content.lower()
+    contents = content.split()
+    isGreyIn = False
+    isDeathIn = False
+    isNotIn = False
+
+    for words in contents :
+        if ('grey' in words or 'gray' in words) :
+            isGreyIn = True
+        
+        if ('dead' in words or 'die' in words) :
+            isDeathIn = True
+        
+        if ('alive' in words) :
+            isDeathIn = True
+            isNotIn = not isNotIn
+        
+        if ('not' in words or "n\'t" in words):
+            if words == "notn\'t" :
+                continue
+            isNotIn = not isNotIn
+    
+    return isGreyIn and isDeathIn and not isNotIn
 
 @bot.command()
 async def test(ctx, text):
@@ -23,11 +52,29 @@ async def greypatpat(ctx):
     await ctx.message.delete()
     await ctx.send("<a:patpat:790589898724868097>")
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong! {0}ms".format(round(bot.latency, 1)))
+
+@bot.event
+async def on_message(message):
+    print("activated onmessage")
+    #check the message is not from itself
+    if message.author == bot.user:
+        return
+    
+    if grey_death_checker(message) :
+        print("activated process")
+        i = random.randint(0, len(NoU)-1)
+        await message.channel.send(NoU[i])
+    await bot.process_commands(message)
+
 @bot.event
 async def on_ready():
     print('logging in')
     print('connection was succesful')
     await bot.change_presence(status=discord.Status.online, activity=None)
 
-bot.run(os.environ['token'])
 
+#bot.run(os.environ['token'])
+bot.run('NzkwNTcxNTUyMzQ1MDMwNjg2.X-CjIg.4c1Z7VkXwBSP7RzeGpCjoAHZnTw')
